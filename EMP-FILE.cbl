ff8408450034224c4537bd3e868d7da85341134a
mouhamed92 <hamamimohamed92@gmail.com>
2023-05-31 13:44:53 +0200

    write file

--- EMP-FILE.cbl
+++ EMP-FILE.cbl
@@ -15,9 +15,14 @@
        FD Employee.
            01 Employee-FILE.
              05 Employee-ID  PIC  9(5).
+             05 FILLER       PIC  X(1).
              05 Name         PIC  X(25).
+             05 FILLER       PIC  X(1).
              05 Date-nes     PIC  X(10).
+             05 FILLER       PIC  X(1).
+             05 FILLER       PIC  X(1).
              05 Salaire      PIC  X(7).
+             05 FILLER       PIC  X(1).
              05 Fonction     PIC  X(20).
 
 
@@ -25,9 +30,13 @@
 
            01 WS-Employee.
              05 WS-Employee-ID  PIC  9(5).
+             05 FILLER       PIC  X(1).
              05 WS-Name         PIC  X(25).
+             05 FILLER       PIC  X(1).
              05 WS-Date-nes     PIC  X(10).
+             05 FILLER       PIC  X(1).
              05 WS-Salaire      PIC  X(7).
+             05 FILLER       PIC  X(1).
              05 WS-Fonction     PIC  X(20).
 
            01 WS-EOF            PIC  A(1).
@@ -44,6 +53,27 @@
                   END-READ
               END-PERFORM
               CLOSE Employee.
+
+
+              Display "DONNER EM-ID"
+              Accept   Employee-ID
+              Display "NOM"
+              Accept   Name
+              Display "DATE NESSANCE"
+              Accept   Date-nes
+              Display "SALAIRE"
+              Accept   Salaire
+              Display "FONCTION"
+              ACCEPT  Fonction
+
+           OPEN EXTEND Employee.
+                 MOVE Employee-ID TO EMPLOYEE-ID.
+                 MOVE Date-nes TO Date-nes.
+                 MOVE Salaire TO Salaire.
+                 MOVE Fonction TO Fonction.
+                 WRITE EMPLOYEE-FILE
+                 END-WRITE.
+           CLOSE EMPLOYEE.
 
               STOP RUN.
 
